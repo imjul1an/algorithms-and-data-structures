d426f6e685aaacd749e949061fc1a4fd670f920a
julianusti <julian.ustiyanovych@gmail.com>
2019-03-02 23:47:02 +0100

    add babel/node export all ds and use them in index.js

--- data-structures/index.js
+++ data-structures/index.js
@@ -1,3 +1,3 @@
 export { createStack as stack } from './stack'
 export { createQueue as queue } from './queue'
-export { createPriorityQueue as priorityQueue } from './prioriityQueue'
+export { createPriorityQueue as priorityQueue } from './priority-queue'
---
+++ index.js
@@ -0,0 +1,9 @@
+import { stack, queue, priorityQueue } from './data-structures'
+
+const s = stack()
+const q = queue()
+const pq = priorityQueue()
+
+console.log(s.isEmpty())
+console.log(q.isEmpty())
+console.log(pq.isEmpty())
